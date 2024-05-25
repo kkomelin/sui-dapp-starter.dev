4e501e9e9476d8ec78b3894b344b1f1dd74b1075
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-25 08:57:59 +0700

    Added search.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -213,6 +213,21 @@ const config: Config = {
     },
   ],
   plugins: [tailwindPlugin],
+  themes: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      },
+    ],
+  ],
 }
 
 export default config;
