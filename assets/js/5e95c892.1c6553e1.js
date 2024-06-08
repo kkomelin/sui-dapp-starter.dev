334ab187a29c4b668da543144c11a83cc615e50b
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-06-08 17:18:00 +0200

    Deploy website - based on 440865c09f800f7d58a326ac884c70f08041b98a

---
+++ assets/js/5e95c892.1c6553e1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksui_dapp_starter_dev=self.webpackChunksui_dapp_starter_dev||[]).push([[647],{9804:(e,s,r)=>{r.r(s),r.d(s,{default:()=>n});r(758);var a=r(3526),t=r(2832),u=r(6535),d=r(389),p=r(6087),c=r(6070);function n(e){return(0,c.jsx)(t.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,c.jsx)(p.A,{children:(0,d.v)(e.route.routes)})})}}}]);
